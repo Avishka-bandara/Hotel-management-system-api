c32ea0e1e63649e43056f5fa01a6bc5a844d0247
AvishkaBandara <avishkabandara2001@gmail.com>
2025-08-04 10:14:48 +0530

    initial commit

---
+++ src/main/java/com/cpd/hotel_sytstem/Hotel_management_system_api/HotelManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.cpd.hotel_sytstem.Hotel_management_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementSystemApiApplication.class, args);
+	}
+
+}
